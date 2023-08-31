884e229d6753fa2fc283cdbbc3aa120f2921a58d
Joshua Chinwendu <joshuachinwendu@gmail.com>
2023-08-31 10:37:31 +0100

    put back the PORT

--- server.js
+++ server.js
@@ -33,4 +33,4 @@ app.use('/api/users', userRoutes);
 
 
 app.get('/', (req,res) => res.send(`Server is up and running`));
-app.listen();
+app.listen(port, () => console.log(`Server started on port ${port}`));
